84751813fe9b79a2f2b383475d4f5f9dfa041832
Relizc <3464840574@qq.com>
2023-04-22 13:33:49 +0800

    SUBMIT YOUR COMMAND PACKAGE FIRST

--- src/main/java/net/itsrelizc/industrial/Main.java
+++ src/main/java/net/itsrelizc/industrial/Main.java
@@ -1,13 +1,11 @@
 package net.itsrelizc.industrial;
 
-import Commands.IndustrialGive;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
         System.out.println("[RSIndustrial] SMP-Industrial enabled");
-        getCommand("industrialgive").setExecutor(new IndustrialGive());
     }
 
 }
