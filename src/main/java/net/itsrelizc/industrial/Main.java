41ea12b60cb6d0cd5c7ff702dcc8ac80e10847f5
Relizc <3464840574@qq.com>
2023-05-01 12:50:54 +0800

    dick

--- src/main/java/net/itsrelizc/industrial/Main.java
+++ src/main/java/net/itsrelizc/industrial/Main.java
@@ -1,11 +1,19 @@
 package net.itsrelizc.industrial;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.itsrelizc.workblocks.BasicWorkBlock;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
         System.out.println("[RSIndustrial] SMP-Industrial enabled");
+        
+        BasicWorkBlock work = new BasicWorkBlock(new Location(Bukkit.getWorld("world"), 0, 63, 0));
+        
+        
     }
 
 }
